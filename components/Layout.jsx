1a9a4b87a59fcbc9e007cf99498d04245bdf6704
walidmeguenni <walidmg125@gmail.com>
2022-06-18 21:11:30 +0200

    13 commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <div className="layout">
       <Head>
         <title>MG Store</title>
-        <link rel="shortcut  icon" type="image/png" href="../public/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
         <NavBar />
